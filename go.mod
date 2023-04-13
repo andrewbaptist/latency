d49e6acda317cb6432f2cabe04a4c1494c6e9e2c
Andrew Baptist <baptist@cockroachlabs.com>
2023-04-13 13:11:19 -0400

    Cleanups

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/andrewbaptist/latency
 
 go 1.19
 
-require github.com/gordonklaus/portaudio v0.0.0-20221027163845-7c3b689db3cc // indirect
+require github.com/gordonklaus/portaudio v0.0.0-20221027163845-7c3b689db3cc
--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"github.com/gordonklaus/portaudio"
 	"math"
 	"math/rand"
 	"net"
 	"sort"
-	"time"
-
-	"github.com/gordonklaus/portaudio"
 )
 
 // This code intentionally avoids as much heap allocation as possible by
@@ -17,7 +15,7 @@ import (
 
 // Store the last 256 points
 // TODO: Initialize all to 0.
-var data [256]uint32
+var data [256]float64
 var dataCounter byte
 var totalPoints int64
 var counter = 0
@@ -36,8 +34,7 @@ func getPercentiles() [8]float64 {
 	for i := range steps {
 		// Take the sorted value at 1/2^i so ends up with the
 		// 1, 2, 4, ... 128 values
-		pxValue := math.Max(float64(d[1<<i]), 1)
-		// TODO: Take rate into account
+		pxValue := math.Max(d[1<<i], 1)
 		// Store in reverse order for easier computation later. We base our rate
 		// on the P50 (128th) value.
 		// Store the inverse of the rate in microseconds.
@@ -55,16 +52,13 @@ const rate = 44100
 // 1ms -> 23Hz
 // 100ms -> 69Hz
 func convertLatencyToStep(micro float64) float64 {
-	rawStep := 20 * math.Log1p(micro)
+	rawStep := 30 * math.Log1p(micro)
 	normal := math.Min(math.Max(rawStep, 100.0), 400.0)
 	// Normalize based on the sound base rate.
 	return normal / rate
 }
 
-var lastCounter int
-var lastTime time.Time
-
-// var multipliers = []float64{1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0}
+// Choose "harmonic notes"
 var multipliers = []float64{1.0, 5.0 / 4, 4.0 / 3, 3.0 / 2, 5.0 / 3, 2.0, 5.0 / 2, 3.0}
 
 // This is called repeatedly with a "small window" of time. We need to fill
@@ -78,10 +72,6 @@ func genAudio(out []float32) {
 	}
 
 	baseStep := convertLatencyToStep(percentiles[0])
-	if counter == 100 {
-		fmt.Println(percentiles)
-		fmt.Println("Base freq: ", int(baseStep*rate))
-	}
 	prevP := 0.0
 
 	var i int
@@ -99,27 +89,32 @@ func genAudio(out []float32) {
 		// adjacent step. Start with amp 1 for the base, and increase for the
 		// others. Don't allow any individual amp to get above 2.0 (your ears
 		// will thank me).
-		amp := math.Min(p/prevP, 2.0) - 1
-		if counter == 100 {
-			fmt.Print(amp, ",")
-		}
+		amp := math.Min(p/prevP, 3.0) - 1
 		prevP = p
+
+		if counter%1000 == 0 {
+			fmt.Printf("%d: %0.2f (%0.2f)\n", i, amp, p)
+		}
 
 		for j := range out {
 			// get the next output value for this curve and add to the others.
-			out[j] += float32(amp * math.Sin(2*math.Pi*phase[i]))
+			nextOut := amp * math.Sin(2*math.Pi*phase[i])
+			out[j] += float32(nextOut)
 			// move phase along in small steps update for next time, resetting
 			// to 0 so that we can avoid wild swings when we change the step size.
 			// Phase always stays between 0 and 1.
 			_, phase[i] = math.Modf(phase[i] + step)
 		}
 	}
+	if counter%1000 == 0 {
+		fmt.Println("Base freq: ", int(baseStep*rate))
+	}
 	counter++
 	//	println(out[0])
 
 }
 
-// This is a long listening stream
+// This listens for data and updates the data array with it.
 func listenDataUDP() {
 	// Listen for UDP messages on port 12345
 	addr, err := net.ResolveUDPAddr("udp", ":12345")
@@ -146,35 +141,12 @@ func listenDataUDP() {
 		}
 		value := binary.LittleEndian.Uint32(buf[0:4])
 		// This is a data race with the reader, but we don't care too much...
-		// since we have a single writer and we are writing single values.
-		data[dataCounter] = value
+		// since we are the single writer and we are writing single values.
+		data[dataCounter] = float64(value)
 		// This will auto-wrap at 256 which is why we chose byte type.
 		dataCounter += 1
 		totalPoints += 1
 	}
-}
-func test() {
-	err := portaudio.Initialize()
-	if err != nil {
-		panic(err)
-	}
-	defer portaudio.Terminate()
-	for i := range data {
-		data[i] = uint32(rand.NormFloat64()*100000 + 100000)
-	}
-
-	stream, err := portaudio.OpenDefaultStream(0, 1, rate, 8192, genAudio)
-	if err != nil {
-		panic(err)
-	}
-	defer stream.Close()
-	err = stream.Start()
-	if err != nil {
-		panic(err)
-	}
-
-	// Run forever
-	time.Sleep(5 * time.Second)
 }
 
 func main() {
@@ -185,13 +157,12 @@ func main() {
 	defer portaudio.Terminate()
 	// Initialize all our points to 0 to start.
 	for i := range data {
-		//		data[i] = uint32(rand.NormFloat64()*10000000 + 10000000)
-		data[i] = 0
+		data[i] = rand.NormFloat64()*10000 + 10000
+		//data[i] = 0
 	}
-
 	go listenDataUDP()
 
-	stream, err := portaudio.OpenDefaultStream(0, 1, rate, 0, genAudio)
+	stream, err := portaudio.OpenDefaultStream(0, 1, rate, 1024, genAudio)
 	if err != nil {
 		panic(err)
 	}
